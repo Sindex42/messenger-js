8af84c272ac918b601bf3eeedec6f915b4e9db02
Connor Kam-Cheong <connorkc42@gmail.com>
2019-01-17 16:28:26 +0000

    Messages construct with content and formatted time

--- spec/MessageSpec.js
+++ spec/MessageSpec.js
@@ -1,5 +1,20 @@
 /* global describe, it, expect */
+'use strict'
 
 describe('Message', function () {
+  let message
+  let time
 
+  beforeEach(function () {
+    time = new Date(1484524800000)
+    message = new Message("test message", time)
+  })
+
+  it('should create a message with content', function () {
+    expect(message.content).toEqual("test message")
+  })
+
+  it('should create a message with a time of creation', function () {
+    expect(message.time).toEqual('00:00:00')
+  })
 })
--- src/Message.js
+++ src/Message.js
@@ -1,5 +1,8 @@
-class Message {
-  constructor() {
+'use strict'
 
+class Message {
+  constructor(content, time = new Date) {
+    this.content = content,
+    this.time = `${time.toLocaleDateString()} ${time.toLocaleTimeString()}`
   }
 }
